35ef52b4dd94160e3640b26aaa51f963d13506a4
jcohen66 <jcohen66@optonline.net>
2019-01-19 23:02:20 -0500

    Added SpringBootConfiguration to test case.

--- src/test/java/com/example/orikademo/MappingDirectApplicationTests.java
+++ src/test/java/com/example/orikademo/MappingDirectApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.orikademo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MappingDirectApplicationTests {
